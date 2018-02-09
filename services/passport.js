c42bb6d01e0a36d2ec50ca614049d5df7a2d1f72
Chenqi <zhaochenqi2013@gmail.com>
2018-02-09 11:08:12 -0500

    try old python version

--- services/passport.js
+++ services/passport.js
@@ -24,17 +24,15 @@ passport.use(
 			callbackURL: '/auth/google/callback',
 			proxy: true
 		}, 
-		(accessToken, refreshToken, profile, done) => {
-			User.findOne({ googleId: profile.id}).then(existingUser => {
-					if (existingUser) {
-						//console.log("already exist")
-						done(null, existingUser); 
-					} else {
-						new User({ googleId: profile.id })
-						.save()
-						.then(user => done(null, user));
-					}
-				});
+		async (accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({ googleId: profile.id});
+			if (existingUser) {
+				// console.log("already exist")
+				// find user, return
+				return done(null, existingUser); 
+			}
+			const user = await new User({ googleId: profile.id }).save();
+			done(null, user);
 		}
 	)
 );
